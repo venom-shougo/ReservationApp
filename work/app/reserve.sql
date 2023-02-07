e07c1a6042f4f4d00a5a7414740eced446ef2b36
venom-shougo <wb.work.atelier@gmail.com>
2023-02-07 21:21:02 +0900

    機能開発タスクを作成

--- work/app/reserve.sql
+++ work/app/reserve.sql
@@ -1,41 +1,31 @@
 DROP TABLE IF EXISTS reservation;
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users(
-    id INT NOT NULL AUTO_INCREMENT,
-    register_token varchar(128),
-    register_token_sent_at DATETIME,
-    register_token_verified_at DATETIME,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    last_name varchar(32),
-    first_name varchar(32),
-    last_name_kana varchar(32),
-    first_name_kana varchar(32),
-    password varchar(128),
-    email varchar(256),
-    postcode varchar(8),
-    prefectures varchar(16),
-    city varchar(256),
-    block varchar(256),
-    building varchar(128),
-    phone_number varchar(32),
-    status ENUM("tentative", "public") NOT NULL DEFAULT "tentative",
+CREATE TABLE shop (
+    id INT NOT NULL AOUT_INCREMENT,
+    shop_name varchar(128) NOT NULL,
+    shop_id varchar(16) NOT NULL,
+    email vvarchar(256) NOT NULL,
+    password INT NOT NULL,
+    reservable_date DATE NOT NUL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    max_reserve_num INT NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation(
-    id INT NOT NULL AUTO_INCREMENT,
-    users_id INT,
-    room_number varchar(16),
-    regist_at DATETIME DEFAULT NOW(),
-    reserve_at DATE,
-    reserve_at_time TIME,
-    price DECIMAL(10,3),
-    pay varchar(32),
-    cancel_at DATETIME DEFAULT NOW(),
-    PRIMARY KEY (id),
-    FOREIGN KEY (users_id) REFERENCES users(id)
+CREATE TABLE reservation (
+    id INT NOT NULL AOUT_INCREMENT,
+    reserve_date DATE NOT NULL,
+    reserve_time TIME NOT NULL,
+    reserve_num INT NOT NULL,
+    name varchar(100) NOT NULL,
+    email varchar(254) NOT NULL,
+    tel varchar(20) NOT NULL,
+    comment vmediumtext,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
 );
 
 INSERT INTO users (email) VALUES("beat0729pp1@gamil.com");
