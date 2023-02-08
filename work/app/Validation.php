2906c7cf212b9b2e0e97cc8646a425683a034bb9
venom-shougo <wb.work.atelier@gmail.com>
2023-02-08 23:06:04 +0900

    各入力項目のバリデーション実装

--- work/app/Validation.php
+++ work/app/Validation.php
@@ -1,20 +1,59 @@
 <?php
-
-class ValidateForm
+namespace Reserve\Validation;
+class Validation
 {
     /**
-     * エントリーバリデーション
-     * @param array $email
-     * @return array $err
+     * 予約者入力検証
+     * @param array $input
+     * @return array
      */
-    public static function setEntry($email)
+    public static function inputValueCheck(array $input): array
     {
-        $err = [];
-        $validateemail = trim(filter_var($email['email'], FILTER_VALIDATE_EMAIL));
-        if (empty($validateemail)) {
-        $err['email'] = 'メールアドレスを入力してください';
+        $error = [];
+
+        if (empty($input['reserve_date'])) {
+            $error['reserve_date_err'] = '予約日を選択して下さい';
         }
-        return $err;
+        // TODO予約日はプリダウン設定値を決定後にバリデーション実装
+
+        if (empty($input['reserve_num'])) {
+            $error['reserve_num_err'] = '人数を選択して下さい';
+        } elseif (!preg_match('/^[0-9]+$/', $input['reserve_num'])) {
+            $error['reserve_num_err'] = '人数を正しく入力して下さい';
+        }
+
+        if (empty($input['reserve_time'])) {
+            $error['reserve_time_err'] = '予約時間を選択して下さい';
+        }
+        // TODO予約時間はプリダウン設定値を決定後にバリデーション実装
+
+        if (empty(trim($input['name']))) {
+            $error['name_err'] = '名前を入力して下さい';
+        } elseif (mb_strlen($input['name'], 'utf-8') > 20) {
+            $error['name_err'] = '名前をは20文字以内で入力して下さい';
+        }
+
+        if (empty(trim($input['email']))) {
+            $error['email_err'] = 'メールアドレスを入力して下さい';
+        } elseif (trim(mb_strlen($input['email'], 'utf-8')) > 100) {
+            $error['email_err'] = 'メールアドレスは100文字以内で入力して下さい';
+        } elseif (trim(!filter_var($input['email'], FILTER_VALIDATE_EMAIL))) {
+            $error['email_err'] = 'メールアドレスが不正です';
+        }
+
+        if (empty(trim($input['tel']))) {
+            $error['tel_err'] = '電話番号を入力して下さい';
+        } elseif (trim(mb_strlen($input['tel'], 'utf-8')) > 20) {
+            $error['tel_err'] = '電話番号は20文字以内で入力して下さい';
+        } elseif (trim(!preg_match('/^\d{2,4}-\d{2,4}-\d{4}$/', $input['tel']))) {
+            $error['tel_err'] = '電話番号正しく入力して下さい';
+        }
+
+        if (mb_strlen($input['comment'], 'utf-8') > 2000) {
+            $error['comment_err'] = '備考欄は2000文字以内で入力して下さい';
+        }
+
+        return $error;
     }
 
     /**
