4c6386713d2c8076d03f04a9394dfa95640243f3
venom-shougo <wb.work.atelier@gmail.com>
2023-02-10 20:27:40 +0900

    入力内容をセッションから取り出し、画面に表示させる#10

--- work/public/confirm.php
+++ work/public/confirm.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once(__DIR__ . '/../app/config.php');
+
 include('_header.php');
 ?>
 <header class="text-white bg-dark text-center p-3">SAMPEL SHOP</header>
@@ -9,27 +12,27 @@ include('_header.php');
         <tbody>
             <tr>
                 <th scope="row">日時</th>
-                <td>2023年2月2日（木）12時00分</td>
+                <td><?= Utils::h($_SESSION['reserve']['reserve_date']); ?> <?= Utils::h($_SESSION['reserve']['reserve_time']); ?></td>
             </tr>
             <tr>
                 <th scope="row">人数</th>
-                <td>4名</td>
+                <td><?= Utils::h($_SESSION['reserve']['reserve_num']); ?>人</td>
             </tr>
             <tr>
                 <th scope="row">氏名</th>
-                <td>西本正吾</td>
+                <td><?= Utils::h($_SESSION['reserve']['name']); ?></td>
             </tr>
             <tr>
                 <th scope="row">メールアドレス</th>
-                <td>beat.0729.pp1@gmail.com</td>
+                <td><?= Utils::h($_SESSION['reserve']['email']); ?></td>
             </tr>
             <tr>
                 <th scope="row">電話番号</th>
-                <td>080-3054-8336</td>
+                <td><?= Utils::h($_SESSION['reserve']['tel']); ?></td>
             </tr>
             <tr>
                 <th scope="row">備考</th>
-                <td colspan="2">飲み放題を付けてください</td>
+                <td colspan="2"><?= nl2br(Utils::h($_SESSION['reserve']['comment'])); ?></td>
                 </tr>
         </tbody>
     </table>
