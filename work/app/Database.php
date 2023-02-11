4033e7875d657b06aaa29cb49b8e0a69f70926d7
venom-shougo <wb.work.atelier@gmail.com>
2023-02-11 14:19:43 +0900

    予約日のプルダウン実装

--- work/app/Database.php
+++ work/app/Database.php
@@ -20,4 +20,21 @@ class Database
         exit;
         }
     }
+
+    public static function getShopData(string $shopId): array|bool
+    {
+        $result = false;
+        $sql = "SELECT * FROM shops WHERE id = :id";
+        try {
+            $pdo = self::connect();
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindValue(':id', $shopId, \PDO::PARAM_STR);
+            $stmt->execute();
+            $shopId = $stmt->fetch();
+            return $shopId;
+        } catch(\PDOException $e) {
+            echo 'データ取得失敗' . $e->getMessage();
+            return $result;
+        }
+    }
 }
